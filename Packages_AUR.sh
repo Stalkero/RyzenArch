efd3d3f076e9726304294c4a6af053aeb752f21c
Stalkero <selteab@interia.pl>
2020-11-20 00:49:44 +0100

    Replaced md files with shell scripts

---
+++ Packages_AUR.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+yay -S \
+davinci-resolve  \
+droidcam \
+freedownloadmanager \
+jmtpfs \
+lightdm-webkit-theme-aether \
+minecraft-launcher \ 
+polybar \
+protontricks-git \
+spotify \
+xampp \
+teams \
+ttf-font-awesome \
+ttf-meslo-nerd-font-powerlevel10k  \
+zsh-theme-powerlevel10k-git
---
+++ Packages_Official.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+
+
+#Make you sure enable multilib in your pacman configuration
+
+#/etc/pacman.conf
+
+#[multilib]
+
+#Include = /etc/pacman.d/mirrorlist
+
+## Official
+sudo pacstrap /mnt \
+base \
+base-devel \
+adobe-source-code-pro-fonts \
+amd-ucode \
+android-tools \
+android-udev \
+blueman \
+bzip2 \
+cabextract \
+cantarell-fonts \
+chromium \
+code \
+cuda \
+cups \
+dconf-editor \
+f2fs-tools \
+feh \
+ffmpeg \
+freetype2 \
+gimp \
+git \
+gnome-calculator \
+gnome-calendar \
+gnome-weather \
+gnu-free-fonts \
+gnutls \
+go \
+gparted \
+gsfonts \
+gvfs-mtp \
+gzip \
+htop \
+i3 \
+imagemagick \
+imv \
+jre-openjdk \
+jre-openjdk-headless \
+lib32-gnutls \
+lib32-nvidia-utils \
+lib32-vulkan-icd-loader \
+libguestfs \
+libreoffice-fresh \
+lightdm \
+lightdm-webkit2-greeter \
+linux-api-headers \
+linux \
+linux-headers \
+linux-zen \
+linux-zen-headers \
+lsd \
+lutris \
+mediainfo \
+mtpfs \
+nautilus \
+neofetch \
+neovim \
+networkmanager \
+noto-fonts-emoji \
+nvidia-dkms \
+nvidia-settings \
+nvidia-utils \
+obs-studio \
+opencl-nvidia \
+openvpn \
+p7zip \
+pavucontrol \
+picom \
+playerctl \
+powerline-fonts \
+pulseaudio \
+pulseeffects \
+python \
+python2 \
+qemu \
+rofi \
+scrot \
+splix \
+steam \
+sudo \
+ttf-liberation \
+ttf-opensans \
+unicode-character-database \
+unzip \
+v4l-utils \
+v4l2loopback-dkms \
+vim \
+virt-manager \
+vlc \
+vulkan-icd-loader \
+wget \
+wine \
+winetricks \
+xorg-xinput \
+zsh \
+zsh-autosuggestions \
+zsh-syntax-highlighting 
